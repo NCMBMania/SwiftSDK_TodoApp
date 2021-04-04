2d709fc6719b5187a8a41961e580a36a21119431
Atsushi <atsushi@moongift.jp>
2021-04-05 08:31:57 +0900

    Initial Commit

---
+++ todoapp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  todoapp
+//
+//  Created by Atsushi on 2021/04/05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ todoapp/todoappApp.swift
@@ -0,0 +1,17 @@
+//
+//  todoappApp.swift
+//  todoapp
+//
+//  Created by Atsushi on 2021/04/05.
+//
+
+import SwiftUI
+
+@main
+struct todoappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
